f2451cd31eefe2b84520080e13e27825fa120cc0
zuhran110 <35305@students.riphah.edu.pk>
2026-04-03 17:59:35 +0500

    index update

--- server/index.ts
+++ server/index.ts
@@ -91,13 +91,10 @@ app.get("/", (_req, res) => {
   res.send("CMS Backend is running");
 });
 
-app.use(
-  (err: unknown, _req: Request, res: Response, _next: NextFunction) => {
-    const message =
-      err instanceof Error ? err.message : "Internal Server Error";
-    res.status(500).json({ error: message });
-  },
-);
+app.use((err: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  const message = err instanceof Error ? err.message : "Internal Server Error";
+  res.status(500).json({ error: message });
+});
 
 if (!isVercel) {
   ensureDbConnection()
